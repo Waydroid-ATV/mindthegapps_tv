62f1ac50da87a238af2d13c95ad076c2708233a2
supechicken <me@supechicken666.dev>
2024-12-08 10:47:27 +0000

    Partially revert "Remove x86/x86_64 support"
    Only bring back x86_64
    
    This reverts commit 59398dddc714e251604bb7e77447fe87ed26cb7a.
    
    Signed-off-by: supechicken <me@supechicken666.dev>

--- build/main.mk
+++ build/main.mk
@@ -24,3 +24,7 @@ gapps_tv_arm_minimal:
 gapps_tv_arm64_minimal:
 	@echo "Compiling GApps without GMS Launcher for arm64..."
 	@bash $(BUILD_GAPPS) arm64 minimal 2>&1
+
+gapps_tv_x86_64:
+	@echo "Compiling GApps for x86_64..."
+	@bash $(BUILD_GAPPS) x86_64 2>&1
